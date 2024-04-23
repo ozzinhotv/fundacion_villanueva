73be3824b0a8c15744bacf0bf918e7b4c6ef2f79
ozzinhotv <142857305+ozzinhotv@users.noreply.github.com>
2024-04-23 04:16:25 +0000

    1er dia. Se trabajo con el Navbar y la vista Home

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -3,15 +3,17 @@ import { Link } from "react-router-dom";
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-light bg-light mb-3">
+	  <nav className="navbar navbar-light bg-light mb-5 ">
+		<div className="container d-flex">
 			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
+				<span className="navbar-brand mb-0 h1 lead"><strong>Fundación Villanueva</strong></span>  
 			</Link>
-			<div className="ml-auto">
-				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
-				</Link>
-			</div>
-		</nav>
+			<div className="vidaNavbar h3 lead"> Vida</div>
+			<div className="obraNavbar h3 lead"> Obra</div>
+			<div className="escritosNavbar h3 lead"> Escritos</div>
+			<div className="archivosNavbar h3 lead"> Archivos</div>
+			<div className="fundacionNavbar h3 lead"> Fundación</div>
+		</div>	
+	  </nav>
 	);
-};
+  };
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -3,13 +3,21 @@ import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
 
 export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
+	<div className="mt-5 m-5">
+		<div className="d-flex grid">
+			<div className="homeImg g-col-6">
+				<p>
+					<img src={rigoImage} />
+				</p>
+			</div>
+			<div className="g-col-6">
+				<h1 className="bigTitle lead"> Fundación Villanueva</h1>
+				<div className="homeDescription lead">
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam quis pulvinar massa, vestibulum cursus sapien. Mauris non tellus sem. Nunc eget arcu in ligula venenatis tincidunt. Curabitur varius turpis nec ex ultricies porta. Nunc blandit quam sit amet ante imperdiet, non finibus nisl condimentum. Nam at ante sodales, gravida ipsum nec, imperdiet libero. Sed scelerisque nisi non neque aliquet facilisis sit amet et tortor. Mauris dictum, leo vel bibendum commodo, lorem nisi hendrerit magna, eu aliquet risus erat in ante. Cras tincidunt ut tellus ac ornare. Curabitur scelerisque lectus vel laoreet volutpat. Ut sed tincidunt metus. Aliquam lobortis purus sit amet dui dictum, id tristique massa condimentum. Ut congue vel nibh vel lobortis. Proin at quam dignissim, suscipit tellus a, ultricies lectus. Proin auctor, ante vitae malesuada pretium, mi leo maximus lorem, vel ultricies enim odio at urna. Morbi ac arcu tincidunt, tincidunt erat in, fermentum sem.
+					Aliquam fermentum ligula at sollicitudin ullamcorper.
+					</p>
+				</div>
+			</div>
+		</div>
 	</div>
 );
